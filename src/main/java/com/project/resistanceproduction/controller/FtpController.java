8c52c2bd6201f3c1b61984da5938de3ccd6b8275
qazws <qazwsxedc219@qq.com>
2023-03-29 15:30:45 +0800

    修改类名

--- src/main/java/com/project/resistanceproduction/controller/FtpController.java
+++ src/main/java/com/project/resistanceproduction/controller/FtpController.java
@@ -1,39 +1,35 @@
 package com.project.resistanceproduction.controller;
 
-import com.project.resistanceproduction.Utils.FtpUtil;
+import com.project.resistanceproduction.ftpconfig.FtpConfigProperties;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.net.ftp.FTPClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/ftp")
 public class FtpController {
     @Autowired
-    private FtpUtil ftpUtils;
+    private FtpConfigProperties ftpConfigProperties;
 
-    /**
-     */
-    @RequestMapping("/test")
-    public void FtpTest(HttpServletResponse response, MultipartFile file) throws IOException {
+    @RequestMapping("/downFtpFile")
+    public void downFtpFile(){
+        FTPClient ftpClient = ftpConfigProperties.connectFtp();
+        // 路径必须是对应登录账号访问权限的路径  否则会识别不到文件
+        // useradd -d /etc/vsftpd/data xiangjiao  创建账户 xiangjiao，并设置其访问的ftp服务路径为 /etc/vsftpd/data
+        //  passwd xiangjiao  给账户  xiangjiao 设置密码
+        ftpConfigProperties.downloadFile(ftpClient,"/info","222.txt","E:\\JAVA_WORKSPACE\\ftpWorkDir");
+    }
 
-        String path = StringUtils.EMPTY;
-        String name = "222.txt";
-        //下载getFtpImage
-        FtpUtil.downLoad(name,path,response);
-      //  FtpUtil.getFtpImage(response,path);
-
-        OutputStream out =  response.getOutputStream();
-
+    @RequestMapping("/uploadFtpFile")
+    public void uploadFtpFile(){
+        FTPClient ftpClient = ftpConfigProperties.connectFtp();
+        ftpConfigProperties.uploadFile(ftpClient,"/etc/vsftpd/data","2233.txt","E:\\study\\springboot-stydy\\doc\\22.txt");
     }
 
 
