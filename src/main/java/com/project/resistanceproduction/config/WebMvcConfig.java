a23251bdc023fb07ffc97ea5a3a20790d5169899
colatoice <524543892@qq.com>
2023-03-30 16:45:00 +0800

    图片静态资源服务器

---
+++ src/main/java/com/project/resistanceproduction/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.project.resistanceproduction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName WebMvcConfig
+ * @Description 图片静态资源服务器映射
+ * @Author sd
+ * @Date 2023/03/30 15:59
+ * @Version 1.0
+ */
+@Configuration
+public class WebMvcConfig  implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /images/**是静态映射， file:/root/images/是文件在服务器的路径
+        registry.addResourceHandler("/images/**.*")
+                .addResourceLocations("file:D:\\img\\");
+    }
+}
